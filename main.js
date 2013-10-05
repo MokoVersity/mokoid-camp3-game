c826dc6917a42db599ce2dc5c0ab3451c9f258f1
Jollen Chen <jollen@moko365.com>
2013-10-05 11:05:10 +0800

    Add: random x, y and r

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
 var bubble = (function() {
-	var canvas,
-		ctx;
+	var ballX,
+		ballY,
+		radi;
+
+	var timeoutVar,
+		counter;
+
+	function drawBall() {
+		var canvas = document.getElementById('board');
+
+		var ctx = canvas.getContext('2d');
+
+		canvas.width = 480;
+		canvas.height = 640;
+		
+		ballX = Math.floor(Math.random() * 300);
+		ballY = Math.floor(Math.random() * 500);
+		radi = Math.floor(Math.random() * 100) + 30;
+
+		ctx.fillStyle = 'black';
+		ctx.beginPath();
+		ctx.arc(ballX, ballY, radi, 0, Math.PI * 2, true);
+		ctx.fill();
+
+		if (counter >= 10) {
+			clearTimeout(timeoutVar);
+		} else {
+			counter = counter + 1;
+			timeoutVar = setTimeout(drawBall, 1000);
+		}
+	}
 
 	function start() {
-		canvas = document.getElementById('board');
+		var canvas = document.getElementById('board');
 
-		ctx = canvas.getContext('2d');
+		var ctx = canvas.getContext('2d');
 
 		canvas.width = 480;
 		canvas.height = 640;
 
+		ballX = Math.floor(Math.random() * 300);
+		ballY = Math.floor(Math.random() * 500);
+		radi = Math.floor(Math.random() * 100) + 30;
+
 		ctx.fillStyle = 'black';
 		ctx.beginPath();
-		ctx.arc(100, 100, 100, 0, Math.PI * 2, true);
+		ctx.arc(ballX, ballY, radi, 0, Math.PI * 2, true);
 		ctx.fill();
+
+		counter = 1;
+
+		timeoutVar = setTimeout(drawBall, 1000);
 	}
 
 	return {
