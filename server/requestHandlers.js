c1d25ff2a5423ef815ed86494beea8f4da5e047a
Jollen Chen <jollen@moko365.com>
2013-10-12 13:52:37 +0800

    Improve code quality

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -10,18 +10,21 @@ function chat(req, res, query, clients) {
 	var parsedstring = querystring.parse(query);
 
 	console.log('chat is called');
-	console.log('Message: ' + parsedstring.m);
+	console.log('Message: ' + parsedstring.content);
 
 	var obj = {
 		type: 'message',
 		data: {
-			message: parsedstring.m,
+			message: parsedstring.content,
 			username: 'jollen', 
 			timestamp: (new Date()).getTime()
 		}
 	};
 
 	history.push(obj);
+
+	// reduce history[]
+	history.slice();
 
 	// Real-time push message
 	for (i = 0; i < clients.length; i++) {
